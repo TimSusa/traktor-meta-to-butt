15530791785780119a0cc068fc61b4a7ab12661e
TimSusa <timmsusa@googlemail.com>
2022-01-13 02:06:56 +0100

    asf

---
+++ start.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+icecast -c icecast.xml & 
+
+open -a butt &
+
+./spawnTrackInfo.sh
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+ls /Volumes/timsusa/Documents/Native\ Instruments/Traktor\ 3.5.1/History || ( read h; echo ${h:42} )
